d8d2833b6911f873a4bdef3a04e6a29670355ee8
Kelebra <ricardokelebra@gmail.com>
2020-03-20 10:13:58 -0600

    User create

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const userRoutes = require('./routes/user');
+require('dotenv/config');
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/user', userRoutes);
+
+
+//Home Route
+app.get('/', (req, res) =>{
+    res.send('Home');
+});
+
+
+
+//Conext to Mongo
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('All rigth to conecto DB');
+});
+
+app.listen(4000); 
--- index.js
+++
@@ -1,1 +0,0 @@
-const api ='Empezando el API'
